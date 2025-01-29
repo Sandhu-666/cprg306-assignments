ba81e8bddc158481ddd06c08cc5bfff0c987d4af
Sahil Sandhu <sahilpreetsingh1@edu.sait.ca>
2025-01-28 23:10:05 -0700

    Updated file

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Sahilpreet Singh</p>
+            <p>
+             <Link href="https://github.com/Sandhu-666"> Link to Repo </Link>   
+            </p>    
+        </div>
+    )
+}
