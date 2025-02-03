3ca1b70c377466e71be6f70e8f51fe5454232c7b
Sahil Sandhu <sahilpreetsingh1@edu.sait.ca>
2025-02-03 09:10:05 -0700

    updated bg color

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ItemList from './item-list';
 
 const Page = () => (
-  <main className="p-6 bg-white min-h-screen">
-    <h1 className="text-3xl font-bold text-center mb-6 text-black">Shopping List</h1>
+  <main className="p-6 bg-black min-h-screen">
+    <h1 className="text-3xl font-bold text-center mb-6 text-white">Shopping List</h1>
     <ItemList />
   </main>
 );
